82efc1f424b40e505d78fdf0432c615ae16f9e97
Michael Micco <mmicco@godaddy.com>
2010-10-19 14:36:48 +0000

    Moved ContextWrapper to Obsolete
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/Obsolete;C153345

---
+++ Atlantis.Framework.ContextWrapper/Atlantis.Framework.ContextWrapper/HttpContextWrapper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Atlantis.Framework.ContextWrapper
+{
+  public static class HttpContextWrapper
+  {
+
+/* THIS SHOULD ONLY BE USED IN DEBUG
+ * Put your code that references the Mock features in a #if DEBUG block 
+ */
+#if DEBUG
+
+    private static bool _useMockContext = false;
+    private static string _mockRequestPage = "null.html";
+    private static string _mockRequestUrl = "http://localhost/null/null.html";
+    private static string _mockRequestQueryString = "?mock=true";
+
+    [ThreadStatic]
+    private static System.Web.HttpContext _mockContext;
+
+    /// <summary>
+    /// This property can only be used in DEBUG.  Put your code that uses it in a #if DEBUG block.
+    /// It is only designed for unit tests
+    /// </summary>
+    public static bool UseMockContext
+    {
+      get { return _useMockContext; }
+      set { _useMockContext = value; }
+    }
+
+    /// <summary>
+    /// This property can only be used in DEBUG.  Put your code that uses it in a #if DEBUG block.
+    /// It is only designed for unit tests
+    /// </summary>
+    public static string MockRequestPage
+    {
+      get { return _mockRequestPage; }
+      set
+      {
+        _mockRequestPage = value;
+        ResetMockContext();
+      }
+    }
+
+    /// <summary>
+    /// This property can only be used in DEBUG.  Put your code that uses it in a #if DEBUG block.
+    /// It is only designed for unit tests
+    /// </summary>
+    public static string MockRequestUrl
+    {
+      get { return _mockRequestUrl; }
+      set
+      {
+        _mockRequestUrl = value;
+        ResetMockContext();
+      }
+    }
+
+    /// <summary>
+    /// This property can only be used in DEBUG.  Put your code that uses it in a #if DEBUG block.
+    /// It is only designed for unit tests
+    /// </summary>
+    public static string MockRequestQueryString
+    {
+      get { return _mockRequestQueryString; }
+      set
+      {
+        _mockRequestQueryString = value;
+        ResetMockContext();
+      }
+    }
+
+    /// <summary>
+    /// This method can only be used in DEBUG.  Put your code that uses it in a #if DEBUG block.
+    /// It is only designed for unit tests
+    /// </summary>
+    public static void ResetMockContext()
+    {
+      _mockContext = null;
+    }
+
+#endif // DEBUG
+
+    public static System.Web.HttpContext Current
+    {
+      get
+      {
+        System.Web.HttpContext result = System.Web.HttpContext.Current;
+
+#if DEBUG
+        if (_useMockContext)
+        {
+          if (_mockContext == null)
+          {
+            StringWriter writer = new StringWriter();
+            HttpRequest request = new HttpRequest(_mockRequestPage, _mockRequestUrl, _mockRequestQueryString);
+            HttpResponse response = new HttpResponse(writer);
+            _mockContext = new System.Web.HttpContext(request, response);
+
+            ConstructorInfo[] consInfo = typeof(HttpSessionState).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] args = new object[1];
+            args[0] = new MockSession();
+            object oSession = consInfo[0].Invoke(args);
+            HttpSessionState session = (HttpSessionState)oSession;
+            _mockContext.Items["AspSession"] = session;
+          }
+
+          result = _mockContext;
+        }
+#endif // DEBUG
+
+        return result;
+      }
+    }
+  }
+}
---
+++ Atlantis.Framework.ContextWrapper/Atlantis.Framework.ContextWrapper/MockSession.cs
@@ -0,0 +1,163 @@
+﻿#if DEBUG
+using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Atlantis.Framework.ContextWrapper
+{
+	public class MockSession : NameObjectCollectionBase, IHttpSessionState
+	{
+		private string _sessionId = new Guid().ToString();
+
+		internal MockSession()
+		{
+			this.Clear();
+		}
+
+		#region IHttpSessionState Members
+
+		public void Abandon()
+		{
+			this.Clear();
+		}
+
+		public void Add(string name, object value)
+		{
+			BaseAdd(name, value);
+		}
+
+		public void Clear()
+		{
+			BaseClear();
+		}
+
+		public int CodePage
+		{
+			get
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+			set
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+		}
+
+		public HttpCookieMode CookieMode
+		{
+			get { return HttpCookieMode.UseCookies; }
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		public bool IsCookieless
+		{
+			get { return false; }
+		}
+
+		public bool IsNewSession
+		{
+			get { return false; }
+		}
+
+		public bool IsSynchronized
+		{
+			get { return true; }
+		}
+
+		public int LCID
+		{
+			get
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+			set
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+		}
+
+		public SessionStateMode Mode
+		{
+			get { return SessionStateMode.Custom; }
+		}
+
+		public void Remove(string name)
+		{
+			BaseRemove(name);
+		}
+
+		public void RemoveAll()
+		{
+			BaseClear();
+		}
+
+		public void RemoveAt(int index)
+		{
+			BaseRemoveAt(index);
+		}
+
+		public string SessionID
+		{
+			get { return _sessionId; }
+		}
+
+		public HttpStaticObjectsCollection StaticObjects
+		{
+			get { throw new Exception("The method or operation is not implemented."); }
+		}
+
+		public object SyncRoot
+		{
+			get { throw new Exception("The method or operation is not implemented."); }
+		}
+
+		public int Timeout
+		{
+			get
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+			set
+			{
+				throw new Exception("The method or operation is not implemented.");
+			}
+		}
+
+		public object this[int index]
+		{
+			get
+			{
+				return BaseGet(index);
+			}
+			set
+			{
+				BaseSet(index, value);
+			}
+		}
+
+		public object this[string name]
+		{
+			get
+			{
+				return BaseGet(name);
+			}
+			set
+			{
+				BaseSet(name, value);
+			}
+		}
+
+		#endregion
+
+		public new bool IsReadOnly
+		{
+			get { return false; }
+		}
+	}
+}
+#endif // DEBUG
