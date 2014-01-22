39ada52e88e8afed908ae9cd37c67256f92bd36c
Michael Micco <mmicco@godaddy.com>
2014-01-22 21:55:52 +0000

    PLSignupInfo triplet moved to obsolete (request and response live in PrivateLabel triplet library now)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/Obsolete;C389220

---
+++ Atlantis.Framework.PLSignupInfo/Atlantis.Framework.PLSignupInfo.Impl/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Atlantis.Framework.PLSignupInfo.Impl")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+#endif
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GoDaddy.com, LLC")]
+[assembly: AssemblyProduct("Atlantis.Framework.PLSignupInfo.Impl")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com, LLC 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fcfdc265-cc9d-4a43-9c31-708cd8da33dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.12.4.3")]
---
+++ Atlantis.Framework.PLSignupInfo/Atlantis.Framework.PLSignupInfo.Tests/PLSignupInfoTests.cs
@@ -0,0 +1,32 @@
+﻿using Atlantis.Framework.PLSignupInfo.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.PLSignupInfo.Tests
+{
+  [TestClass]
+  public class PLSignupInfoTests
+  {
+    [TestMethod]
+    [DeploymentItem("Interop.gdDataCacheLib.dll")]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("Atlantis.Framework.PLSignupInfo.Impl.dll")]
+    public void BasicSignupInfoOff()
+    {
+      PLSignupInfoRequestData request = new PLSignupInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 1724);
+      PLSignupInfoResponseData response = (PLSignupInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 522);
+      Assert.IsNotNull(response.DefaultTransactionCurrencyType);
+    }
+
+    [TestMethod]
+    [DeploymentItem("Interop.gdDataCacheLib.dll")]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("Atlantis.Framework.PLSignupInfo.Impl.dll")]
+    public void BasicSignupInfoOn()
+    {
+      PLSignupInfoRequestData request = new PLSignupInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 281896);
+      PLSignupInfoResponseData response = (PLSignupInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 522);
+      Assert.IsNotNull(response.DefaultTransactionCurrencyType);
+    }
+
+  }
+}
